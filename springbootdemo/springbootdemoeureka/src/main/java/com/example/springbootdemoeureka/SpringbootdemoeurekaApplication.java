7ce1e4c826f31b92b4507e5b595aab8a004eac17
caochun <caochun9729@163.com>
2020-10-30 17:21:07 +0800

    springCloud spring2.2.1 eureka Hoxton.M8 fegin远程调用

---
+++ springbootdemo/springbootdemoeureka/src/main/java/com/example/springbootdemoeureka/SpringbootdemoeurekaApplication.java
@@ -0,0 +1,18 @@
+package com.example.springbootdemoeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author cc
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootdemoeurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoeurekaApplication.class, args);
+    }
+
+}
